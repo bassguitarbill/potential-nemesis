69b22b174118b25b54f1bf65b8cf30ba0f3a96a7
BassguitarBill <bassguitarbill@gmail.com>
2014-05-29 22:26:28 -0400

    first commit

---
+++ background.js
@@ -0,0 +1,36 @@
+var bg = [];
+var fg = [];
+var time = new Date();
+var lastFrame = new Date();
+var fps = 0;
+
+var bgImage = new Image();
+bgImage.src = "images/bg.png";
+bg.push(bgImage);
+
+var fgImage = new Image();
+fgImage.src = "images/man.png";
+fg.push(fgImage);
+
+function bgDraw(ctx){
+	bg.forEach(function(image,b,c){
+		ctx.drawImage(image,0,0,image.width,image.height,0,0,ctx.canvas.width,ctx.canvas.height);
+	});
+}
+
+function fgDraw(ctx){
+	fg.forEach(function(image){
+		ctx.drawImage(image,(new Date() - time) / 20,0);
+	});
+}
+
+var draw = setInterval(function(){
+	fps = (Math.round(1000/(new Date() - lastFrame)) + " FPS");
+	lastFrame = new Date();
+	bgDraw(ctx);
+	fgDraw(ctx);
+},1000/60);
+
+var updateFps = setInterval(function(){
+	document.getElementById("fps").textContent = fps;
+},1000);
